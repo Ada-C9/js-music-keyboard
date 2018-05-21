af8bc43770ff5f19ea158568d5455dbb5893e8c8
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-05-21 16:26:08 -0700

    add play(tone)

--- noise.js
+++ noise.js
@@ -1,3 +1,13 @@
 $(document).ready( function() {
   // your code here
+  $('.instrument > .note').click(function() {
+    let tone = $(this).html();
+    play(tone);
+  });
+
+  function play(tone) {
+      let audio = document.getElementById(tone + 'Audio');
+      audio.load();
+      audio.play();
+    }
 });
