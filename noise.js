369bd2ad827b4144af02af313c2bb3dfbbe42f7a
Kate Pond <kate@thepondsedge.com>
2018-05-21 16:37:53 -0700

    plays the music by keypress and click

--- noise.js
+++ noise.js
@@ -1,3 +1,14 @@
 $(document).ready( function() {
-  // your code here
+  // https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/jquery-events.md
+
+  $('.instrument').on('click', 'button', function(event) {
+    $(`#${$(this).html()}Audio`).get(0).load();
+    $(`#${$(this).html()}Audio`).get(0).play();
+  });
+
+  $('body').keydown((event) => {
+    $(`#${event.key}Audio`).get(0).load();
+    $(`#${event.key}Audio`).get(0).play();
+  });
+
 });
