b74a4455ec0af9c37593566680d34e7dc933fd79
Mary Lamkin <mmlamkin@gmail.com>
2018-05-21 16:35:39 -0700

    adds refactored code for click events

--- noise.js
+++ noise.js
@@ -1,72 +1,89 @@
 $(document).ready( function() {
 
-  $('.c').click( function() {
-    $('#cAudio').get()[0].play();
-  });
+  // $('.c').click( function() {
+  //   $('#cAudio').get()[0].play();
+  // });
+  //
+  // $('body').keydown( function(event) {
+  //   if (event.keyCode === 67 ) {
+  //     $('#cAudio').get()[0].play();
+  //   }
+  // });
+  //
+  // $('.d').click( function() {
+  //   $('#dAudio').get()[0].play();
+  // });
+  //
+  // $('body').keydown( function(event) {
+  //   if (event.keyCode === 68 ) {
+  //     $('#dAudio').get()[0].play();
+  //   }
+  // });
+  //
+  // $('.e').click( function() {
+  //   $('#eAudio').get()[0].play();
+  // });
+  //
+  // $('body').keydown( function(event) {
+  //   if (event.keyCode === 69 ) {
+  //     $('#eAudio').get()[0].play();
+  //   }
+  // });
+  //
+  // $('.f').click( function() {
+  //   $('#fAudio').get()[0].play();
+  // });
+  //
+  // $('body').keydown( function(event) {
+  //   if (event.keyCode === 70 ) {
+  //     $('#fAudio').get()[0].play();
+  //   }
+  // });
+  //
+  // $('.g').click( function() {
+  //   $('#gAudio').get()[0].play();
+  // });
+  //
+  // $('body').keydown( function(event) {
+  //   if (event.keyCode === 71 ) {
+  //     $('#gAudio').get()[0].play();
+  //   }
+  // });
+  //
+  // $('.a').click( function() {
+  //   $('#aAudio').get()[0].play();
+  // });
+  //
+  // $('body').keydown( function(event) {
+  //   if (event.keyCode === 65 ) {
+  //     $('#aAudio').get()[0].play();
+  //   }
+  // });
+  //
+  // $('.b').click( function() {
+  //   $('#bAudio').get()[0].play();
+  // });
+  //
+  // $('body').keydown( function(event) {
+  //   if (event.keyCode === 66 ) {
+  //     $('#bAudio').get()[0].play();
+  //   }
+  // });
 
-  $('body').keydown( function(event) {
-    if (event.keyCode === 67 ) {
-      $('#cAudio').get()[0].play();
-    }
-  });
+  const NOTES = ['a','b', 'c', 'd', 'e', 'f', 'g']
 
-  $('.d').click( function() {
-    $('#dAudio').get()[0].play();
-  });
+  for (let i = 0; i < NOTES.length; i++) {
+    let note = NOTES[i]
+    $(`.${note}`).click( function() {
+      $(`#${note}Audio`).get()[0].play();
+    })
+  }
 
-  $('body').keydown( function(event) {
-    if (event.keyCode === 68 ) {
-      $('#dAudio').get()[0].play();
-    }
-  });
+  // $('body').keydown( function(event) {
+  //   if (event.keyCode === 66 ) {
+  //     $('#bAudio').get()[0].play();
+  //   }
+  // });
 
-  $('.e').click( function() {
-    $('#eAudio').get()[0].play();
-  });
 
-  $('body').keydown( function(event) {
-    if (event.keyCode === 69 ) {
-      $('#eAudio').get()[0].play();
-    }
-  });
-
-  $('.f').click( function() {
-    $('#fAudio').get()[0].play();
-  });
-
-  $('body').keydown( function(event) {
-    if (event.keyCode === 70 ) {
-      $('#fAudio').get()[0].play();
-    }
-  });
-
-  $('.g').click( function() {
-    $('#gAudio').get()[0].play();
-  });
-
-  $('body').keydown( function(event) {
-    if (event.keyCode === 71 ) {
-      $('#gAudio').get()[0].play();
-    }
-  });
-
-  $('.a').click( function() {
-    $('#aAudio').get()[0].play();
-  });
-
-  $('body').keydown( function(event) {
-    if (event.keyCode === 65 ) {
-      $('#aAudio').get()[0].play();
-    }
-  });
-
-  $('.b').click( function() {
-    $('#bAudio').get()[0].play();
-  });
-
-  $('body').keydown( function(event) {
-    if (event.keyCode === 66 ) {
-      $('#bAudio').get()[0].play();
-    }
-  });
 });
