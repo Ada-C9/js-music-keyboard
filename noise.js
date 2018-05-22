2671e14ada823f62de98d8da45f8f8774026365b
Alexandria Brown <axbn2190@gmail.com>
2018-05-21 17:11:20 -0700

    can play all notes using on click method

--- noise.js
+++ noise.js
@@ -1,7 +1,15 @@
 $(document).ready( function() {
 
-  $('.note.c').click( function() {
-    $('#cAudio')[0].play();
+
+
+  // $('.note.c').click( function() {
+  //   $('#cAudio')[0].play();
+  // })
+
+  $('.instrument').on('click', 'button', function(event) {
+    $('#' + this.innerHTML + 'Audio')[0].play();
   })
+
+
 
 });
