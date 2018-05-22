0f86297f8af53029c7940d026434913fde40cb38
AngelaPoland <angelalpoland@gmail.com>
2018-05-21 18:02:13 -0700

    didn't commit while making changes but now clicks and keys make the sounds

--- noise.js
+++ noise.js
@@ -1,9 +1,21 @@
 $(document).ready( function() {
 
-  $('.c').click(function() {
-    // alert('clicked on c');
-    $('#cAudio').get(0).load();
-    $('#cAudio').get(0).play();
-    })
+  // $('.c').click(function() {
+  //   $('#cAudio').get(0).load();
+  //   $('#cAudio').get(0).play();
+  //   })
 
-});
+
+  $('.note').click(function() {
+    let clickSound = $('#' + $(this).html() + 'Audio');
+    clickSound.get(0).load();
+    clickSound.get(0).play();
+  })
+
+  $('body').keydown(function(event){
+    let keySound = $(`#${event.key}Audio`)
+    keySound.get(0).load();
+    keySound.get(0).play();
+  })
+
+}); //end of document.ready
