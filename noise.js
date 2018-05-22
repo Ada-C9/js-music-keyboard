c16de2e4ba9a020cd8c460f7350f0511289fb3a2
Alexandria Brown <axbn2190@gmail.com>
2018-05-21 22:32:55 -0700

    can play all notes when corresponding key is pressed

--- noise.js
+++ noise.js
@@ -1,15 +1,16 @@
 $(document).ready( function() {
 
-
-
-  // $('.note.c').click( function() {
-  //   $('#cAudio')[0].play();
-  // })
-
-  $('.instrument').on('click', 'button', function(event) {
+  $('.instrument').on('click', 'button', function() {
     $('#' + this.innerHTML + 'Audio')[0].play();
   })
 
+  $('.instrument').keypress( function(event) {
+    $('#' + event.key + 'Audio')[0].play();
 
+  })
 
 });
+
+// $('.note.c').click( function() {
+//   $('#cAudio')[0].play();
+// })
