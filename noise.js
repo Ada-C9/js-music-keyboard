8b4cbac3cbe1f79dded366bf5ffb94ca688ec4de
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-05-21 17:11:46 -0700

    working with lower-case key-depressions now, and I don't think I'm going to worry about the upper-case ones at this juncture, unless I'm just spilling over with extra time tomorrow morning. Pull request time.

--- noise.js
+++ noise.js
@@ -1,6 +1,8 @@
 $(document).ready( function() {
   // your code here
 
+  // Clicking.
+
   $( '.c' ).click(function() {
     $('audio#cAudio')[0].play()
   });
@@ -29,11 +31,23 @@ $(document).ready( function() {
     $('audio#bAudio')[0].play()
   });
 
+// key depressions. a is 65, g is 71, the rest are obvious
+
+    $('body').keydown((event) => {
+      if (event.key == 'a') {
+        $('audio#aAudio')[0].play();
+      } else if (event.key == 'b') {
+        $('audio#bAudio')[0].play();
+      } else if (event.key == 'c') {
+        $('audio#cAudio')[0].play();
+      } else if (event.key == 'd') {
+        $('audio#dAudio')[0].play();
+      } else if (event.key == 'e') {
+        $('audio#eAudio')[0].play();
+      } else if (event.key == 'f') {
+        $('audio#fAudio')[0].play();
+      } else if (event.key == 'g') {
+        $('audio#gAudio')[0].play();
+      }
+    });
 });
-
-
-
-
-// $( "#other" ).click(function() {
-//   $( "#target" ).click();
-// });
