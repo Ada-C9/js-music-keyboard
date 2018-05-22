1c2ef8968c39034c50e1dc304cb691ca2a0af0eb
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-21 22:49:02 -0700

    refactored

--- noise.js
+++ noise.js
@@ -1,22 +1,17 @@
-// const stopAllAudio = function stopAllAudio() {
-//   $('audio').trigger('pause').prop('currentTime', 0);
-// };
+const stopAllAudio = function stopAllAudio() {
+  $('audio').trigger('pause').prop('currentTime', 0);
+};
+
+let playAudio = function playAudio(targetSound) {
+  stopAllAudio();
+  $(`#${targetSound + 'Audio'}`).trigger('play');
+};
 
 $(document).ready( function() {
-  // $('body').on('click', 'button', function(event) {
   $('body').keydown(function(event){
-    if (event.keyCode >= 65 && event.keyCode <= 71) {
-      // stopAllAudio();
-      $('audio').trigger('pause').prop('currentTime', 0);
-      let soundTarget = event.key + "Audio";
-      $(`#${soundTarget}`).trigger('play');
-    }
+    if (event.keyCode >= 65 && event.keyCode <= 71) { playAudio(event.key) }
   });
 
-  $('button').on('click', function(event) {
-    $('audio').trigger('pause').prop('currentTime', 0);
-    let soundTarget = event.currentTarget.classList[1] + "Audio";
-    $(`#${soundTarget}`).trigger('play');
-  });
+  $('button').on('click', function(event) { playAudio(event.currentTarget.classList[1]); });
 
 });
