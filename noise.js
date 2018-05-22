636b3ff58574dbf6a402de907f76d03cb23d96e9
Alexandria Brown <axbn2190@gmail.com>
2018-05-21 22:42:11 -0700

    styling and cleanup

--- noise.js
+++ noise.js
@@ -1,16 +1,9 @@
-$(document).ready( function() {
-
+$(document).ready(() => {
   $('.instrument').on('click', 'button', function() {
     $('#' + this.innerHTML + 'Audio')[0].play();
-  })
+  });
 
   $('.instrument').keypress( function(event) {
     $('#' + event.key + 'Audio')[0].play();
-
-  })
-
-});
-
-// $('.note.c').click( function() {
-//   $('#cAudio')[0].play();
-// })
+  });  
+})
