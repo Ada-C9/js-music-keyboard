fd116ad29021e16a3477b2e9076a49b456400533
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-05-21 21:44:10 -0700

    minimum requirements done

--- noise.js
+++ noise.js
@@ -1,3 +1,22 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(() => {
+
+  const playNote = (noteID) => {
+    const $note = $(`#${noteID}`);
+    $note[0].load();
+    $note[0].play();
+    console.log(`${noteID[0]} note played`);
+  };
+
+  const $instrument = $('.instrument');
+  const $body = $('body');
+
+  $instrument.on('click', '.note', function click() {
+    const noteID = `${this.innerHTML}Audio`;
+    playNote(noteID);
+  });
+
+  $body.keydown((event) => {
+    const noteID = `${String.fromCharCode(event.keyCode).toLowerCase()}Audio`;
+    playNote(noteID);
+  });
 });
