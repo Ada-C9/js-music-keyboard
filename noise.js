1a3a5aa2cc665aae975a92a8bead1fae54686dd6
Phoebe <phoebe.manning8@gmail.com>
2018-05-22 08:57:48 -0700

    has keys and click events

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
-  // your code here
+  $('div.instrument').on('click', 'button', function(){
+    console.log($(this).html());
+    $( '#'+$(this).html()+'Audio' ).get(0).play();
+  });
+
+  let keyBindings = { 67:'c', 68:'d', 69:'e', 70:'f', 71:'g', 65:'a', 66:'b' }
+
+  $('body').keydown( function(event){
+    if ( Object.keys(keyBindings).includes( event.keyCode.toString() ) ){
+      $( '#' + keyBindings[event.keyCode] + 'Audio' ).get(0).play();
+    }
+  });
+
 });
