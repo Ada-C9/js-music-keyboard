c849cca26bc5d99fe0b28cd03b3a7fe9446904ed
Nora Peters <norapeters5@gmail.com>
2018-05-21 23:39:05 -0700

    click and keydown event listeners added with playSound helper function

--- noise.js
+++ noise.js
@@ -1,3 +1,19 @@
+const playSound = function(letter) {
+  let sound = $(`#${letter}Audio`)[0];
+  if (sound) {
+    sound.currentTime = 0;
+    sound.play();
+  }
+}
+
 $(document).ready( function() {
-  // your code here
+  $('.note').click(function(event) {
+    const letter = event.target.className.split(" ")[1];
+    playSound(letter);
+  });
+
+  $(document).keydown(function(event) {
+    const letter = event.key;
+    playSound(letter);
+  });
 });
