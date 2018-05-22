10a7e1f8c53f9002aa9cb512938ae28429587361
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-21 17:42:56 -0700

    added .load to the logic so that you don't need to wait for the sound to finish playing before playing it again

--- noise.js
+++ noise.js
@@ -2,10 +2,12 @@ $(document).ready( function() {
 
   $('.note').click( function(){
     const note = $( this ).attr("class").split(" ")[1];
+    $(`#${note}Audio`)[0].load();
     $(`#${note}Audio`)[0].play();
   });
 
   $('body').keydown( function(event){
+    $(`#${event.key}Audio`)[0].load();
     $(`#${event.key}Audio`)[0].play();
   });
 
