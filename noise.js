6a53ac0ee13146f2a352b4f42967f470fa952434
marikoja <Marikoanderson@gmail.com>
2018-05-21 23:01:27 -0700

    notes play on click

--- noise.js
+++ noise.js
@@ -1,3 +1,29 @@
 $(document).ready( function() {
-  // your code here
+  $( '.c' ).click(function() {
+    $('#cAudio')[0].play();
+  });
+
+  $( '.d' ).click(function() {
+    $('#dAudio')[0].play();
+  });
+
+  $( '.e' ).click(function() {
+    $('#eAudio')[0].play();
+  });
+
+  $( '.f' ).click(function() {
+    $('#fAudio')[0].play();
+  });
+
+  $( '.g' ).click(function() {
+    $('#gAudio')[0].play();
+  });
+
+  $( '.a' ).click(function() {
+    $('#aAudio')[0].play();
+  });
+
+  $( '.b' ).click(function() {
+    $('#bAudio')[0].play();
+  });
 });
